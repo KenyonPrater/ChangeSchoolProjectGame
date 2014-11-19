13fc375b9f35b1e2630b07399a377ed8e3af9044
Kenyon Prater <kenyonprater3.14@gmail.com>
2014-11-19 00:18:12 -0800

    Some untested work on tileMapBlender.

--- ChangeGame/src/kenyonprater/com/github/changegame/world/TileMapBlender.java
+++ ChangeGame/src/kenyonprater/com/github/changegame/world/TileMapBlender.java
@@ -11,6 +11,7 @@ public class TileMapBlender {
 	public final int height;
 	
 	private int[][] mapIndex;
+	private int[][] prevMapIndex;
 	private float[][] fadeTime;
 	
 	public TileMapBlender(int w, int h, ArrayList<TiledMap> maplist)
@@ -20,6 +21,7 @@ public class TileMapBlender {
 		maps = maplist;
 		
 		mapIndex = new int[w][h];
+		prevMapIndex = new int[w][h];
 		fadeTime = new float[w][h];
 		
 		for(int i = 0; i < mapIndex.length; i++)
@@ -42,6 +44,20 @@ public class TileMapBlender {
 				for(int j = 0; j < mapIndex[0].length; j++)
 				{
 					maps.get(mapIndex[i][j]).getTileImage(i, j, layer).draw(x+maps.get(0).getTileWidth()*i, y+maps.get(0).getTileHeight()*j);
+				}
+			}
+		}
+	}
+	
+	public void update(double delta)
+	{
+		for(int i = 0; i < mapIndex.length; i++)
+		{
+			for(int j = 0; j < mapIndex[0].length; j++)
+			{
+				if(fadeTime[i][j] != 0)
+				{
+					fadeTime[i][j] = (float) Math.max(fadeTime[i][j]-delta, 0);
 				}
 			}
 		}
